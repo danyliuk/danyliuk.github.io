30ce9dc7fe20b07793c551490dd1fd38d1c374ce
danyliuk <danyliuk.digital@gmail.com>
2017-09-16 01:09:29 +0300

    gdfsds

---
+++ tvereza.pro/js/settings.js
@@ -0,0 +1,5 @@
+var u_layout = "/tvereza.pro/css/layouts/layout_03.css",
+u_theme = "/tvereza.pro/css/themes/theme_01.css",
+u_color = "/tvereza.pro/css/colors/color_04.css",
+u_font = "/tvereza.pro/css/fonts/font_09.css",
+u_background = "/tvereza.pro/css/backgrounds/background_07.css"
