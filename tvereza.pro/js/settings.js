be517a34f11a2287da2f6e09dcc968f4330bbcaa
danyliuk <danyliuk.digital@gmail.com>
2017-09-15 23:47:53 +0300

    set

--- tvereza.pro/js/settings.js
+++ tvereza.pro/js/settings.js
@@ -1,5 +1,5 @@
-var u_layout = "/css/layouts/layout_03.css",
-u_theme = "/css/themes/theme_01.css",
-u_color = "/css/colors/color_04.css",
-u_font = "/css/fonts/font_09.css",
-u_background = "/css/backgrounds/background_07.css"
+var u_layout = "/tvereza.pro/css/layouts/layout_03.css",
+u_theme = "/tvereza.pro/css/themes/theme_01.css",
+u_color = "/tvereza.pro/css/colors/color_04.css",
+u_font = "/tvereza.pro/css/fonts/font_09.css",
+u_background = "/tvereza.pro/css/backgrounds/background_07.css"
