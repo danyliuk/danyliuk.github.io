16bae964beac1267bca77fe3adc96e79f7b3cae1
danyliuk <31867884+danyliuk@users.noreply.github.com>
2018-05-21 23:30:44 +0300

    jhkh

--- projects/js/main.js
+++ projects/js/main.js
@@ -521,7 +521,7 @@
 			initEvents();
 			// Remove loading class from body
 			body.classList.remove('loading');
-			loaders[currentGrid]._render('Shu');
+			loaders[currentGrid]._render('the effect');
 		});
 	}
 
